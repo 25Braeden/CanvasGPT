e46585402419646c6cb2653756487b120e88cc9c
braedenaudlin <baudlin@mocs.flsouthern.edu>
2026-09-15 17:27:13 -0400

    Register TaskItem and AIRequest in the Django admin

--- ai_assistant/admin.py
+++ ai_assistant/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import AIRequest, TaskItem
+
+
+@admin.register(TaskItem)
+class TaskItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'description',
+        'assignment',
+        'user',
+        'estimated_minutes',
+        'order',
+        'is_completed',
+        'ai_generated',
+    )
+    list_filter = ('is_completed', 'ai_generated')
+    search_fields = ('description', 'assignment__title')
+
+
+@admin.register(AIRequest)
+class AIRequestAdmin(admin.ModelAdmin):
+    list_display = ('request_type', 'user', 'requested_at', 'success', 'model_name')
+    list_filter = ('request_type', 'success', 'model_name')
+    search_fields = ('user__username',)
+    readonly_fields = ('requested_at',)
